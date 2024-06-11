bc9dc8c88271e0edad7d3cd4f2860caeb9919705
FaithAdhiambo456 <156072384+FaithAdhiambo456@users.noreply.github.com>
2024-06-11 09:30:45 +0300

    Replace old project with new Angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignupComponent } from './dashboard/signup/signup.component';
+import { LoginComponent } from './dashboard/login/login.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { AboutComponent } from './dashboard/about/about.component';
+import { ContactusComponent } from './dashboard/contactus/contactus.component';
+import { ServicesComponent } from './dashboard/services/services.component';
+import { SearchTextComponent } from './dashboard/search-text/search-text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactusComponent,
+    ServicesComponent,
+    SearchTextComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
